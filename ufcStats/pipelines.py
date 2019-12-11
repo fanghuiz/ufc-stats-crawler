1f15bb0a2cc17ad9e0d01f2eaca29389be16b144
fanghuiz <15682270+fanghuiz@users.noreply.github.com>
2019-12-10 19:36:26 -0500

    Changed pipeline - yielding both info and stats in the same crawl

--- ufcStats/pipelines.py
+++ ufcStats/pipelines.py
@@ -5,7 +5,119 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exporters import JsonLinesItemExporter, CsvItemExporter
+import pathlib
 
-class UfcstatsPipeline(object):
+from .utils import print_time
+
+fields_fight_info = [
+    'fight_id', 'fighter_1', 'fighter_1_id', 'fighter_2', 'fighter_2_id',
+    'winner', 'decision_method', 'fight_duration_lastrnd',
+    'fight_duration_lastrnd_time', 'time_format', 'weight_class', 'date',
+    'location'
+]
+
+fields_fight_stats = [
+    'fight_id',
+    'fighter_id',
+    'fighter_name',
+    'fighter_status',
+    'kd',
+    'n_pass',
+    'n_rev',
+    'n_sub',
+    'sig_str_abs',
+    'sig_str_att',
+    'sig_str_def',
+    'sig_str_land',
+    'total_str_abs',
+    'total_str_att',
+    'total_str_def',
+    'total_str_land',
+    'td_abs',
+    'td_att',
+    'td_def',
+    'td_land',
+    'head_abs',
+    'head_att',
+    'head_def',
+    'head_land',
+    'body_abs',
+    'body_att',
+    'body_def',
+    'body_land',
+    'leg_abs',
+    'leg_att',
+    'leg_def',
+    'leg_land',
+    'distance_abs',
+    'distance_att',
+    'distance_def',
+    'distance_land',
+    'clinch_abs',
+    'clinch_att',
+    'clinch_def',
+    'clinch_land',
+    'ground_abs',
+    'ground_att',
+    'ground_def',
+    'ground_land',
+]
+
+
+class FightSummaryPipeline(object):
+    """
+    Save Fight level summary to csv file
+    """
+    def __init__(self):
+        self.files = {}
+
+    def open_spider(self, spider):
+        time_created = print_time('now')
+        # Create directory
+        path_fight_info = f'data/fight_info'
+        pathlib.Path(path_fight_info).mkdir(parents=True, exist_ok=True)
+        # Write to folder
+        file = open(f'{path_fight_info}/{time_created}.csv', 'wb')
+        self.files[spider] = file
+        self.exporter = CsvItemExporter(file)
+        self.exporter.fields_to_export = fields_fight_info
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+
+class FightStatsPipeline(object):
+    """
+    Save Fight stats to jl file
+    """
+    def __init__(self):
+        self.files = {}
+
+    def open_spider(self, spider):
+        time_created = print_time('now')
+        # Create directory
+        path_fight_stats = f'data/fight_stats'
+        pathlib.Path(path_fight_stats).mkdir(parents=True, exist_ok=True)
+        # Write to folder
+        file = open(f'{path_fight_stats}/{time_created}.jl', 'wb')
+        self.files[spider] = file
+        self.exporter = JsonLinesItemExporter(file)
+        self.exporter.fields_to_export = fields_fight_stats
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
--- ufcStats/spiders/spider.py
+++ ufcStats/spiders/spider.py
@@ -4,9 +4,16 @@ from ..items import *
 from ..utils import *
 
 
-class FightInfoSpider(scrapy.Spider):
-    name = 'fightInfo'
+class FightsSpider(scrapy.Spider):
+    name = 'ufcFights'
     start_urls = ['http://ufcstats.com/statistics/events/completed']
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'ufcStats.pipelines.FightSummaryPipeline': 400,
+            'ufcStats.pipelines.FightStatsPipeline': 410
+        }
+    }
 
     def parse(self, response):
         """
@@ -37,8 +44,10 @@ class FightInfoSpider(scrapy.Spider):
 
     def parse_fight_info(self, response, date, location):
         """
-        Parse fight summary info
+        Parse fight info - fight level summary, and fighter stats
         """
+
+        ##### Fight summary ######
         fight_id = response.url.split('/')[-1]
         # date and location carry over from events page
         date = date.strip()
@@ -61,6 +70,8 @@ class FightInfoSpider(scrapy.Spider):
             winner = fighter_2
         elif status[0].strip() == 'D':
             winner = 'Draw'
+        else:
+            winner = 'NC'
 
         weight_class = response.css(
             '.b-fight-details__fight-title ::text').getall()
@@ -69,9 +80,6 @@ class FightInfoSpider(scrapy.Spider):
             weight_class = weight_class[-1].strip()
         if len(weight_class) == 1:
             weight_class = weight_class[0].strip()
-
-        # TODO: get title bout info from weightclass / fight-title
-        #title_bout = weight_class
 
         decision_method = response.css(
             "i.b-fight-details__text-item_first [style='font-style: normal'] ::text"
@@ -84,7 +92,7 @@ class FightInfoSpider(scrapy.Spider):
         fight_duration_lastrnd_time = fight_details[1].css(
             '::text').getall()[-1]
 
-        l = ItemLoader(item=FightInfoItem(), response=response)
+        l = ItemLoader(item=FightsItem(), response=response)
         l.add_value('fight_id', fight_id)
         l.add_value('date', date)
         l.add_value('location', location)
@@ -94,11 +102,97 @@ class FightInfoSpider(scrapy.Spider):
         l.add_value('fighter_2_id', fighter_2_id)
         l.add_value('winner', winner)
         l.add_value('weight_class', weight_class)
-        #l.add_value('title_bout', title_bout.strip())
         l.add_value('decision_method', decision_method.strip())
         l.add_value('time_format', time_format.strip())
         l.add_value('fight_duration_lastrnd', fight_duration_lastrnd.strip())
         l.add_value('fight_duration_lastrnd_time',
                     fight_duration_lastrnd_time.strip())
+
+        ##### Fighter Stats ######
+        fighter_status = response.css(
+            '.b-fight-details__person-status ::text').getall()
+        fighter_status = [i.strip() for i in fighter_status]
+
+        fighter_id = response.css(
+            '.b-fight-details__person-name ::attr(href)').getall()
+        fighter_id = [i.split('/')[-1] for i in fighter_id]
+
+        stats = response.css('table:not(.js-fight-table)')
+        stats_total = stats[0].css(
+            '.b-fight-details__table-body .b-fight-details__table-col')
+        stats_str = stats[1].css(
+            '.b-fight-details__table-body .b-fight-details__table-col')
+
+        fighter_name = stats_total[0].css('a ::text').getall()
+
+        ## Totals
+        kd = stats_total[1].css('p ::text').getall()
+        kd = [int(i.strip()) for i in kd]
+
+        sig_str = stats_total[2].css('p ::text').getall()
+        total_str = stats_total[4].css('p ::text').getall()
+        td = stats_total[5].css('p ::text').getall()
+
+        n_sub = stats_total[7].css('p ::text').getall()
+        n_sub = [int(i.strip()) for i in n_sub]
+
+        n_pass = stats_total[8].css('p ::text').getall()
+        n_pass = [int(i.strip()) for i in n_pass]
+
+        n_rev = stats_total[9].css('p ::text').getall()
+        n_rev = [int(i.strip()) for i in n_rev]
+
+        ## Significant strikes
+        head = stats_str[3].css('p ::text').getall()
+        body = stats_str[4].css('p ::text').getall()
+        leg = stats_str[5].css('p ::text').getall()
+        distance = stats_str[6].css('p ::text').getall()
+        clinch = stats_str[7].css('p ::text').getall()
+        ground = stats_str[8].css('p ::text').getall()
+
+        #l.add_value('fight_id', fight_id)
+        l.add_value('fighter_id', fighter_id)
+        l.add_value('fighter_name', fighter_name)
+        l.add_value('fighter_status', fighter_status)
+        l.add_value('kd', kd)
+        l.add_value('sig_str_land', get_element_atk(sig_str, 'landed'))
+        l.add_value('sig_str_att', get_element_atk(sig_str, 'attempt'))
+        l.add_value('total_str_land', get_element_atk(total_str, 'landed'))
+        l.add_value('total_str_att', get_element_atk(total_str, 'attempt'))
+        l.add_value('td_land', get_element_atk(td, 'landed'))
+        l.add_value('td_att', get_element_atk(td, 'attempt'))
+        l.add_value('n_sub', n_sub)
+        l.add_value('n_pass', n_pass)
+        l.add_value('n_rev', n_rev)
+        l.add_value('head_land', get_element_atk(head, 'landed'))
+        l.add_value('head_att', get_element_atk(head, 'attempt'))
+        l.add_value('body_land', get_element_atk(body, 'landed'))
+        l.add_value('body_att', get_element_atk(body, 'attempt'))
+        l.add_value('leg_land', get_element_atk(leg, 'landed'))
+        l.add_value('leg_att', get_element_atk(leg, 'attempt'))
+        l.add_value('distance_land', get_element_atk(distance, 'landed'))
+        l.add_value('distance_att', get_element_atk(distance, 'attempt'))
+        l.add_value('clinch_land', get_element_atk(clinch, 'landed'))
+        l.add_value('clinch_att', get_element_atk(clinch, 'attempt'))
+        l.add_value('ground_land', get_element_atk(ground, 'landed'))
+        l.add_value('ground_att', get_element_atk(ground, 'attempt'))
+        l.add_value('sig_str_abs', get_element_dmg(sig_str, 'absorbed'))
+        l.add_value('sig_str_def', get_element_dmg(sig_str, 'defended'))
+        l.add_value('total_str_abs', get_element_dmg(total_str, 'absorbed'))
+        l.add_value('total_str_def', get_element_dmg(total_str, 'defended'))
+        l.add_value('td_abs', get_element_dmg(td, 'absorbed'))
+        l.add_value('td_def', get_element_dmg(td, 'defended'))
+        l.add_value('head_abs', get_element_dmg(head, 'absorbed'))
+        l.add_value('head_def', get_element_dmg(head, 'defended'))
+        l.add_value('body_abs', get_element_dmg(body, 'absorbed'))
+        l.add_value('body_def', get_element_dmg(body, 'defended'))
+        l.add_value('leg_abs', get_element_dmg(leg, 'absorbed'))
+        l.add_value('leg_def', get_element_dmg(leg, 'defended'))
+        l.add_value('distance_abs', get_element_dmg(distance, 'absorbed'))
+        l.add_value('distance_def', get_element_dmg(distance, 'defended'))
+        l.add_value('clinch_abs', get_element_dmg(clinch, 'absorbed'))
+        l.add_value('clinch_def', get_element_dmg(clinch, 'defended'))
+        l.add_value('ground_abs', get_element_dmg(ground, 'absorbed'))
+        l.add_value('ground_def', get_element_dmg(ground, 'defended'))
 
         yield l.load_item()
